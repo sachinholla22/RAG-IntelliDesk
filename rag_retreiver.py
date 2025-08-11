999ef02df74a2dc10f1db61bfe75a5c8d09e3cdc
sachin <sachinholla01@gmail.com>
2025-08-11 17:32:28 +0530

    db connections and table retreival added

--- rag_retreiver.py
+++ rag_retreiver.py
@@ -1,3 +1,36 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+
+import psycopg2
+import os
+from dotenv import load_dotenv
+#connect to postgres
+
+load_dotenv()
+
+
+conn=psycopg2.connect(
+    dbname=os.getenv("DB_NAMES"),
+    user=os.getenv("DB_USERNAMES"),
+    password=os.getenv("DB_PASS"),
+    host=os.getenv("DB_HOSTS"),
+    port=os.getenv("DB_PORTS")
+)
+
+cur=conn.cursor()
+
+cur.execute("SELECT description,title FROM ticket")
+rows=cur.fetchall()
+texts = [f"{title} {description}" for description, title in rows]
+
+embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+vector_store=FAISS.from_texts(texts,embeddings)
+
+vector_store.save_local("faiss_ticket_index")
+
+
+retriever = vector_store.as_retriever()
+
