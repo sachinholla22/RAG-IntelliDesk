240ac0f36f4bd947b671b0f7205101dc42be1ec7
sachin <sachinholla01@gmail.com>
2025-08-12 18:57:22 +0530

    Adding the retrivalQA and debug codes

--- rag_retreiver.py
+++ rag_retreiver.py
@@ -2,15 +2,16 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_huggingface import HuggingFaceEmbeddings
-
+from langchain_mistralai import ChatMistralAI
 import psycopg2
 import os
 from dotenv import load_dotenv
-#connect to postgres
+
 
 load_dotenv()
-
-
+api_key=os.getenv("MISTRAL_AI_KEY")
+llm=ChatMistralAI(api_key=api_key)
+#connect to postgres
 conn=psycopg2.connect(
     dbname=os.getenv("DB_NAMES"),
     user=os.getenv("DB_USERNAMES"),
@@ -34,3 +35,15 @@ vector_store.save_local("faiss_ticket_index")
 
 retriever = vector_store.as_retriever()
 
+qa_chain=RetreivalQA.from_chain_type(
+    llm=llm,
+    retriever=retriever,
+    return_source_documents=True
+)
+
+# 3. Test it
+query = "Show me tickets related to database errors"
+result = qa_chain.invoke({"query": query})
+
+print("Answer:", result["result"])
+print("Sources:", result["source_documents"])
